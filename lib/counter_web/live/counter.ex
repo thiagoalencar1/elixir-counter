a377374636babb7f1f8377fd4775f49f0e31b94e
Thiago Alencar <talencar@gmail.com>
2024-05-22 17:09:12 -0300

    step4: Share state between clients

--- lib/counter_web/live/counter.ex
+++ lib/counter_web/live/counter.ex
@@ -1,16 +1,27 @@
 defmodule CounterWeb.Counter do
   use CounterWeb, :live_view
 
+  @topic "live"
+
   def mount(_params, _session, socket) do
+    CounterWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, :val, 0)}
   end
 
-  def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+  def handle_event("inc", _msg, socket) do
+    new_state = update(socket, :val, &(&1 + 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
   end
 
-  def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
+  def handle_event("dec", _msg, socket) do
+    new_state = update(socket, :val, &(&1 - 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
   end
 
   def render(assigns) do
@@ -18,8 +29,8 @@ defmodule CounterWeb.Counter do
     <div>
       <h1 class="text-4xl font-bold text-center">The count is: <%= @val %></h1>
       <p class="text-center">
-        <.button phx-click="dec">-</.button>
-        <.button phx-click="inc">+</.button>
+        <.button phx-click="dec" class="w-20 bg-red-500 hover:bg-red-600">-</.button>
+        <.button phx-click="inc" class="w-20 bg-green-500 hover:bg-green-600">+</.button>
       </p>
     </div>
     """
